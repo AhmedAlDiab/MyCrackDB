548f766e5516a4dc72e56b430502c2464ad687a6
Ahmed AbdulKarim <54812827+AhmedAlDiab@users.noreply.github.com>
2026-06-24 17:20:16 +0300

    update for linux version

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,19 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class MyCrackDBRecipe(ConanFile):
+    package_type = "application"
     settings = "os", "compiler", "build_type", "arch"
-    
+
+    def configure(self):
+        self.options["rocksdb"].with_zstd = True
+
+    def layout(self):
+        cmake_layout(self)
+
     def requirements(self):
-        self.requires("rocksdb/10.5.1")
-        self.requires("openssl/4.0.1")
+        self.requires("rocksdb/8.10.0")
+        self.requires("openssl/3.2.1")
 
     def generate(self):
         tc = CMakeToolchain(self)
